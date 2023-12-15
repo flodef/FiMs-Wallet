3a804ec5ad5036e63abf23c22413564cc10b51be
Flo <flodef@free.fr>
2023-12-15 14:44:49 +0100

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,11 @@ import Toast from './toast';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'FiMs App',
-  description: 'Application de gestion de jetons FiMs sur Solana.'
+  title: 'FiMs Wallet',
+  description: 'Porte-monnaie FiMs sur Solana.',
 };
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr" className="h-full bg-gray-50">
       <body className="h-full">
