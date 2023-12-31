1f4d9624d5cfad38e0afee8b798cb025f3bbeb46
Flo <flodef@free.fr>
2023-12-31 05:26:07 +0100

    Add a shortAddress function Update function name + test

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -10,7 +10,7 @@ export const IS_LOCAL = !process.env.NEXT_PUBLIC_VERCEL_ENV;
  * @param precision - An integer value indicating the level of precision in the formatted date. The default value is 3.
  * @returns A formatted string representing the date in the format "YYYY-MM-DD" based on the precision value.
  */
-export const GET_FORMATTED_DATE = (date = new Date(), precision = 3) =>
+export const getFormattedDate = (date = new Date(), precision = 3) =>
   !isNaN(date.getTime())
     ? date.getFullYear() +
       (precision > 1 ? '-' + ('0' + (date.getMonth() + 1)).slice(-2) : '') +
@@ -18,16 +18,12 @@ export const GET_FORMATTED_DATE = (date = new Date(), precision = 3) =>
     : '';
 
 // Solana
-export const MEMO_PROGRAM_ID = new PublicKey(
-  'MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr'
-);
+export const MEMO_PROGRAM_ID = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
 export const IS_DEV = process.env.NEXT_PUBLIC_IS_DEV === 'true';
 export const SPL_TOKEN = new PublicKey(
-  IS_DEV
-    ? 'J99D2TvHcev22FF8rNfdUXQx31qzuoVdXRpRiPzJCH6c'
-    : 'Pnsjp9dbenPeFZWqqPHDygzkCZ4Gr37G8mgdRK2KjQp'
+  IS_DEV ? 'J99D2TvHcev22FF8rNfdUXQx31qzuoVdXRpRiPzJCH6c' : 'Pnsjp9dbenPeFZWqqPHDygzkCZ4Gr37G8mgdRK2KjQp'
 );
 export const ENDPOINT = IS_DEV
   ? clusterApiUrl('devnet')
-  : process.env.NEXT_PUBLIC_CLUSTER_ENDPOINT ||
-    'https://solana-mainnet.rpc.extrnode.com';
+  : process.env.NEXT_PUBLIC_CLUSTER_ENDPOINT || 'https://solana-mainnet.rpc.extrnode.com';
+export const getShortAddress = (address: string) => address.slice(0, 4) + '...' + address.slice(-4);
