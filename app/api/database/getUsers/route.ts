dd89a5349b0b4f691ceb4b6390b38adf7bec461a
Flo <flodef@free.fr>
2024-01-05 02:31:57 +0100

    Users should be displayed in proper oder (by id)

--- app/api/database/getUsers/route.ts
+++ app/api/database/getUsers/route.ts
@@ -6,7 +6,9 @@ export async function GET(request: Request) {
   const user = searchParams.get('user');
 
   try {
-    const { rows } = await (user ? sql`SELECT * FROM users WHERE name ILIKE ${user}` : sql`SELECT * FROM users`);
+    const { rows } = await (user
+      ? sql`SELECT * FROM users WHERE name ILIKE ${user}`
+      : sql`SELECT * FROM users ORDER BY id ASC`);
 
     return NextResponse.json(rows);
   } catch (error) {
