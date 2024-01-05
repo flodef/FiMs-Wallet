782a36f5c5839092a2baf19f24cebb3c59a0744b
Flo <flodef@free.fr>
2024-01-05 03:24:22 +0100

    Add transactions to the database

---
+++ app/api/database/getTransactions/route.ts
@@ -0,0 +1,17 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const address = searchParams.get('address');
+
+  try {
+    const { rows } = await (address
+      ? sql`SELECT * FROM transactions WHERE address=${address}`
+      : sql`SELECT * FROM transactions ORDER BY date DESC`);
+
+    return NextResponse.json(rows);
+  } catch (error) {
+    console.error(error);
+  }
+}
