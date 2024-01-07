1ae373f1690e981cd21e8eaf9e64c678625b4eb2
Flo <flodef@free.fr>
2024-01-07 03:49:01 +0100

    Set an alert to test on mobile what's going on at startup 6/n

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ import { useEffect } from 'react';
 import { Page, useNavigation } from './hooks/useNavigation';
 import { usePopup } from './hooks/usePopup';
 import { useUser } from './hooks/useUser';
+import { useIsWindowReady } from './hooks/useWindowParam';
 import { LoadingDot } from './loading';
 import Dashboard from './pages/dashboard';
 import Portfolio from './pages/portfolio';
 import Transactions from './pages/transactions';
 import Users from './pages/users';
-import { useIsWindowReady } from './hooks/useWindowParam';
-import { isMobileDevice } from './utils/mobile';
 
 export default function IndexPage({ searchParams }: { searchParams: { user: string; q: string } }) {
   const { isPopupOpen } = usePopup();
@@ -20,26 +19,19 @@ export default function IndexPage({ searchParams }: { searchParams: { user: stri
   const isWindowReady = useIsWindowReady();
 
   useEffect(() => {
-    let user: string | undefined;
     if (isWindowReady) {
-      if (isMobileDevice()) {
-        const urlSearchParams = new URLSearchParams(window.location.search);
-        user = urlSearchParams.get('user') ?? '';
-      } else {
-        user = searchParams.user;
-      }
-    }
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const user = urlSearchParams.get('user') ?? '';
 
-    alert('user: ' + user);
+      alert('user: ' + user);
 
-    if (user !== undefined) {
       if (user) {
         connect(user);
       } else {
         disconnect();
       }
     }
-  }, [isWindowReady]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [isWindowReady, connect, disconnect]);
 
   return (
     <main className={'space-y-6 p-4 md:p-10 mx-auto max-w-7xl ' + (isPopupOpen ? 'blur-sm' : '')}>
