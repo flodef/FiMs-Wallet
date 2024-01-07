91681e99c58e34dfbaac54ab53203cef1e57f156
Flo <flodef@free.fr>
2024-01-07 03:18:08 +0100

    Set an alert to test on mobile what's going on at startup 2/n

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,29 @@ import Dashboard from './pages/dashboard';
 import Portfolio from './pages/portfolio';
 import Transactions from './pages/transactions';
 import Users from './pages/users';
+import { useIsWindowReady } from './hooks/useWindowParam';
 
 export default function IndexPage({ searchParams }: { searchParams: { user: string; q: string } }) {
   const { isPopupOpen } = usePopup();
   const { connect, disconnect } = useUser();
   const { page } = useNavigation();
 
+  const isWindowReady = useIsWindowReady();
+
   useEffect(() => {
     alert(searchParams.user);
+    if (isWindowReady) {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const userValue = urlSearchParams.get('user');
+      alert(userValue);
+    }
+
     if (searchParams.user) {
       connect(searchParams.user);
     } else {
       disconnect();
     }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [isWindowReady]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <main className={'space-y-6 p-4 md:p-10 mx-auto max-w-7xl ' + (isPopupOpen ? 'blur-sm' : '')}>
