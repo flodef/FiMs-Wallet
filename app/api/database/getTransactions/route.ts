a0798a58b9f6841e43cb04683fa817d690c660c5
Flo <flodef@free.fr>
2024-01-17 22:31:05 +0100

    Add error handling

--- app/api/database/getTransactions/route.ts
+++ app/api/database/getTransactions/route.ts
@@ -13,5 +13,6 @@ export async function GET(request: Request) {
     return NextResponse.json(rows);
   } catch (error) {
     console.error(error);
+    return NextResponse.json(error);
   }
 }
--- app/api/spreadsheet/route.ts
+++ app/api/spreadsheet/route.ts
@@ -1,27 +1,32 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const id = searchParams.get('id');
-    const sheetName = searchParams.get('sheetName');
-    if (!process.env.GOOGLE_API_KEY || (!process.env.GOOGLE_SPREADSHEET_ID && !id) || !sheetName)
-        return NextResponse.json({});
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+  const sheetName = searchParams.get('sheetName');
+  if (!process.env.GOOGLE_API_KEY || (!process.env.GOOGLE_SPREADSHEET_ID && !id) || !sheetName)
+    return NextResponse.json({});
 
-    const isRaw = searchParams.get('isRaw') === 'true';
+  const isRaw = searchParams.get('isRaw') === 'true';
+  try {
     const response = await fetch(
-        `https://sheets.googleapis.com/v4/spreadsheets/${
-            id || process.env.GOOGLE_SPREADSHEET_ID
-        }/values/${sheetName}!A%3AZ?${isRaw ? 'valueRenderOption=UNFORMATTED_VALUE&' : ''}key=${
-            process.env.GOOGLE_API_KEY
-        }`,
-        {
-            headers: {
-                Referer: 'https://www.fims.fi',
-                ContentType: 'application/json',
-            },
-        }
+      `https://sheets.googleapis.com/v4/spreadsheets/${
+        id || process.env.GOOGLE_SPREADSHEET_ID
+      }/values/${sheetName}!A%3AZ?${isRaw ? 'valueRenderOption=UNFORMATTED_VALUE&' : ''}key=${
+        process.env.GOOGLE_API_KEY
+      }`,
+      {
+        headers: {
+          Referer: 'https://www.fims.fi',
+          ContentType: 'application/json',
+        },
+      }
     );
     const data = await response.json();
 
     return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(error);
+  }
 }
