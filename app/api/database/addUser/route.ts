f06b469e0e10ff2982ec2eb4ced3ff8f6db8d090
Flojito <flojito@archireport.com>
2024-02-08 01:19:12 +0100

    feat: add admin page (add user & transaction)

---
+++ app/api/database/addUser/route.ts
@@ -0,0 +1,21 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, address, isPublic } = body;
+
+  if (!name || !address)
+    return NextResponse.json({ error: 'Missing required parameter: name or address.' }, { status: 500 });
+
+  try {
+    const result = await sql`
+      INSERT INTO users (Name, Address, Ispublic)
+      VALUES (${String(name)}, ${String(address)}, ${Boolean(isPublic)})`;
+
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(error);
+  }
+}
