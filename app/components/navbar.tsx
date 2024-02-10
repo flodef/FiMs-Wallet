7437dee926fe84d40a28446cae0c3f7654427e05
Flojito <flojito@archireport.com>
2024-02-10 01:25:40 +0100

    fix: admin navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,11 +3,11 @@
 import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Button, Tab, TabGroup, TabList } from '@tremor/react';
+import { useEffect, useState } from 'react';
 import { FiMsLogo } from '../../public/FiMsLogo';
 import { usePopup } from '../contexts/PopupProvider';
 import { Page, useNavigation } from '../hooks/useNavigation';
 import { useUser } from '../hooks/useUser';
-import { isWindowReady } from '../hooks/useWindowParam';
 import { cls } from '../utils/constants';
 import { Dataset } from '../utils/types';
 
@@ -25,6 +25,12 @@ export default function Navbar() {
   const { page: currentPage, setPage, pages } = useNavigation();
   const { user, isConnected } = useUser();
 
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    setIsAdmin(window.location.pathname === '/admin');
+  }, []);
+
   return (
     <Disclosure
       as="nav"
@@ -35,85 +41,93 @@ export default function Navbar() {
     >
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
-              <div className="flex">
-                <a
-                  className={cls(
-                    'flex flex-shrink-0 items-center',
-                    isConnected && currentPage !== Page.Dashboard ? 'cursor-pointer' : '',
-                  )}
-                  onClick={
-                    isConnected && currentPage !== Page.Dashboard
-                      ? () => setPage(Page.Dashboard)
-                      : isWindowReady && window.location.pathname === '/admin'
-                        ? () => (window.location.href = window.location.origin)
-                        : undefined
-                  }
-                >
-                  <FiMsLogo />
-                </a>
-                <div className={cls('contents', isConnected ? 'animate-display' : 'hidden')}>
-                  <TabGroup
-                    className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8"
-                    style={{ marginTop: '15px' }}
-                    defaultIndex={pages.indexOf(currentPage ?? Page.Dashboard)}
-                    index={pages.indexOf(currentPage ?? Page.Dashboard)}
-                    onIndexChange={i => setPage(pages[i])}
+          {!isAdmin ? (
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="flex h-16 justify-between">
+                <div className="flex">
+                  <a
+                    className={cls(
+                      'flex flex-shrink-0 items-center',
+                      isConnected && currentPage !== Page.Dashboard ? 'cursor-pointer' : '',
+                    )}
+                    onClick={isConnected && currentPage !== Page.Dashboard ? () => setPage(Page.Dashboard) : undefined}
                   >
-                    <TabList>
-                      {pages.map(page => (
-                        <Tab
-                          className={cls(
-                            'text-[20px] hover:text-gray-700 hover:font-bold',
-                            page === currentPage
-                              ? 'cursor-default font-bold border-b-2 mb-0 border-b-blue-500 text-blue-500'
-                              : 'cursor-pointer',
-                          )}
-                          key={page}
-                          aria-current={page === currentPage ? 'page' : undefined}
-                        >
-                          {t[page.toLowerCase()]}
-                        </Tab>
-                      ))}
-                    </TabList>
-                  </TabGroup>
-                  <div className="-mr-2 ml-2 flex items-center sm:hidden">
-                    <Disclosure.Button
-                      className={cls(
-                        'inline-flex items-center justify-center rounded-md p-2 focus:outline-none',
-                        'bg-tremor-background dark:bg-dark-tremor-background',
-                        'text-tremor-content-subtle dark:text-dark-tremor-content-subtle',
-                        'hover:bg-tremor-background-subtle dark:hover:bg-dark-tremor-background-subtle',
-                        'hover:text-tremor-content dark:hover:text-dark-tremor-content',
-                      )}
+                    <FiMsLogo />
+                  </a>
+                  <div className={cls('contents', isConnected ? 'animate-display' : 'hidden')}>
+                    <TabGroup
+                      className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8"
+                      style={{ marginTop: '15px' }}
+                      defaultIndex={pages.indexOf(currentPage ?? Page.Dashboard)}
+                      index={pages.indexOf(currentPage ?? Page.Dashboard)}
+                      onIndexChange={i => setPage(pages[i])}
                     >
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon
-                          className="block h-8 w-8 font-bold focus:border-0 focus:ring-0 focus:outline-0"
-                          aria-hidden="true"
-                        />
-                      ) : (
-                        <Bars3Icon
-                          className="block h-8 w-8 font-bold focus:border-0 focus:ring-0 focus:outline-0"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </Disclosure.Button>
+                      <TabList>
+                        {pages.map(page => (
+                          <Tab
+                            className={cls(
+                              'text-[20px] hover:text-gray-700 hover:font-bold',
+                              page === currentPage
+                                ? 'cursor-default font-bold border-b-2 mb-0 border-b-blue-500 text-blue-500'
+                                : 'cursor-pointer',
+                            )}
+                            key={page}
+                            aria-current={page === currentPage ? 'page' : undefined}
+                          >
+                            {t[page.toLowerCase()]}
+                          </Tab>
+                        ))}
+                      </TabList>
+                    </TabGroup>
+                    <div className="-mr-2 ml-2 flex items-center sm:hidden">
+                      <Disclosure.Button
+                        className={cls(
+                          'inline-flex items-center justify-center rounded-md p-2 focus:outline-none',
+                          'bg-tremor-background dark:bg-dark-tremor-background',
+                          'text-tremor-content-subtle dark:text-dark-tremor-content-subtle',
+                          'hover:bg-tremor-background-subtle dark:hover:bg-dark-tremor-background-subtle',
+                          'hover:text-tremor-content dark:hover:text-dark-tremor-content',
+                        )}
+                      >
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                          <XMarkIcon
+                            className="block h-8 w-8 font-bold focus:border-0 focus:ring-0 focus:outline-0"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <Bars3Icon
+                            className="block h-8 w-8 font-bold focus:border-0 focus:ring-0 focus:outline-0"
+                            aria-hidden="true"
+                          />
+                        )}
+                      </Disclosure.Button>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className={cls('ml-6 flex items-center', currentPage ? 'animate-display' : 'hidden')}>
-                <Button className="flex font-bold" style={{ borderRadius: 24 }} onClick={openPopup}>
-                  {!isConnected ? t.connect : user?.name}
-                </Button>
+                <div className={cls('ml-6 flex items-center', currentPage ? 'animate-display' : 'hidden')}>
+                  <Button className="flex font-bold" style={{ borderRadius: 24 }} onClick={openPopup}>
+                    {!isConnected ? t.connect : user?.name}
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
+          ) : (
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex h-16 justify-between">
+              <a
+                className={cls(
+                  'flex flex-shrink-0 items-center',
+                  isConnected && currentPage !== Page.Dashboard ? 'cursor-pointer' : '',
+                )}
+                onClick={() => (window.location.href = window.location.origin)}
+              >
+                <FiMsLogo />
+              </a>
+            </div>
+          )}
 
-          {isConnected && (
+          {isConnected && !isAdmin && (
             <Disclosure.Panel className="sm:hidden animate-display">
               <div className="space-y-1 pt-2 pb-3">
                 {pages.map(page => (
