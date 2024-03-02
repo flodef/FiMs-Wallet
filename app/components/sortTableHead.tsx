511b33f23be5cacfcac1ba285004e340fb27400e
Flojito <flojito@archireport.com>
2024-03-02 20:44:05 +0100

    fix: filters now handles properly!!

--- app/components/sortTableHead.tsx
+++ app/components/sortTableHead.tsx
@@ -2,25 +2,26 @@ import { ChevronDownIcon, ChevronUpDownIcon, ChevronUpIcon } from '@heroicons/re
 import { Flex, Icon, TableHead, TableHeaderCell, TableRow } from '@tremor/react';
 import { useState } from 'react';
 import { Filter } from '../utils/types';
+import { log } from 'console';
 
 export function SortHeader({
   label,
   index,
+  filters,
   table,
   setTable,
 }: {
   label: string;
   index: number;
+  filters: Filter[];
   table: any[] | undefined;
   setTable: (table: any[] | undefined) => void;
 }) {
-  const [filters, setFilters] = useState<Filter[]>(['descending', 'none', 'none']);
   const changeFilter = (index: number) => {
-    const newFilters = [...filters];
-    newFilters.forEach((filter, i) => {
-      newFilters[i] = i === index ? (filter === 'ascending' ? 'descending' : 'ascending') : 'none';
+    filters.forEach((filter, i) => {
+      filters[i] = i === index ? (filter === 'ascending' ? 'descending' : 'ascending') : 'none';
     });
-    setFilters(newFilters);
+    const filter = filters[index];
 
     if (table?.length) {
       const sortedTable = [...table].sort((a, b) => {
@@ -31,18 +32,23 @@ export function SortHeader({
 
         const getTime = (date: string | number | Date) => new Date(date).getTime();
 
-        if (!isNaN(getTime(propertyA)) && !isNaN(getTime(propertyB))) {
+        if (
+          typeof propertyA === 'object' &&
+          typeof propertyB === 'object' &&
+          !isNaN(getTime(propertyA)) &&
+          !isNaN(getTime(propertyB))
+        ) {
           const numA = getTime(propertyA);
           const numB = getTime(propertyB);
-          return newFilters[index] === 'ascending' ? numA - numB : numB - numA;
+          return filter === 'ascending' ? numA - numB : numB - numA;
         } else if (!isNaN(Number(propertyA)) && !isNaN(Number(propertyB))) {
           const numA = Number(propertyA);
           const numB = Number(propertyB);
-          return newFilters[index] === 'ascending' ? numA - numB : numB - numA;
+          return filter === 'ascending' ? numA - numB : numB - numA;
         } else {
           const stringA = String(propertyA);
           const stringB = String(propertyB);
-          return newFilters[index] === 'ascending' ? stringA.localeCompare(stringB) : stringB.localeCompare(stringA);
+          return filter === 'ascending' ? stringA.localeCompare(stringB) : stringB.localeCompare(stringA);
         }
       });
 
@@ -78,13 +84,20 @@ export default function SortTableHead({
   table: any[] | undefined;
   setTable: (table: any[] | undefined) => void;
 }) {
+  const [filters, _] = useState<Filter[]>(Array.from({ length: labels.length }, () => 'none'));
+
   const length = table?.length ? Object.keys(table[0]).length : 0;
+
   return (
     <TableHead>
       <TableRow>
         {labels.map((label, index) => (
           <TableHeaderCell key={index} className={`w-[${(100 / labels.length).toFixed(6)}%] px-1`}>
-            {index < length ? <SortHeader label={label} index={index} table={table} setTable={setTable} /> : label}
+            {index < length ? (
+              <SortHeader label={label} index={index} table={table} setTable={setTable} filters={filters} />
+            ) : (
+              label
+            )}
           </TableHeaderCell>
         ))}
       </TableRow>
