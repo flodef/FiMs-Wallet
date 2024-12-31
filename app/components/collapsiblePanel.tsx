b760b4d8750b0bae9eee2079bbd05ca0ce3c1940
Flojito <flojito@archireport.com>
2024-12-31 03:05:48 +0100

    fix: collapsible panel not closable

--- app/components/collapsiblePanel.tsx
+++ app/components/collapsiblePanel.tsx
@@ -1,5 +1,6 @@
 import { DownOutlined } from '@ant-design/icons';
 import { Collapse, type CollapseProps } from 'antd';
+import { useEffect, useState } from 'react';
 
 interface CollapsiblePanelProps {
   items: CollapseProps['items'];
@@ -7,10 +8,16 @@ interface CollapsiblePanelProps {
 }
 
 export const CollapsiblePanel = ({ items, isExpanded = true }: CollapsiblePanelProps) => {
+  const [activeKey, setActiveKey] = useState<Array<string | number> | string | number | undefined>();
+
+  useEffect(() => {
+    setActiveKey(isExpanded ? [0] : undefined);
+  }, [isExpanded]);
+
   return (
     <Collapse
       items={items}
-      activeKey={isExpanded ? [0] : undefined}
+      activeKey={activeKey}
       expandIcon={({ isActive }) => (
         <div>
           <DownOutlined style={{ color: 'var(--text)' }} className="text-lg items-center" rotate={isActive ? 180 : 0} />
@@ -19,6 +26,7 @@ export const CollapsiblePanel = ({ items, isExpanded = true }: CollapsiblePanelP
       style={{ backgroundColor: 'var(--background)', color: 'var(--text)' }}
       bordered={false}
       expandIconPosition="end"
+      onChange={setActiveKey}
     />
   );
 };
