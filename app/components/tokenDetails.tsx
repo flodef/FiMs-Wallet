20ec95bee46ec31aeea93f662b7eda7777d2521f
Flojito <flojito@archireport.com>
2025-01-09 04:00:37 +0100

    fix: select default index + refactor

--- app/components/tokenDetails.tsx
+++ app/components/tokenDetails.tsx
@@ -54,7 +54,7 @@ export const TokenDetails = ({
   tokens,
   data,
   total,
-  selectedIndex,
+  selectedIndex = 0,
   onSelectedIndexChange,
 }: TokenDetailsProps) => {
   const { tokenHistoric, setTokenHistoric, tokenHistoricLimit, setTokenHistoricLimit } = useData();
@@ -98,123 +98,120 @@ export const TokenDetails = ({
   const changeToken = useCallback(
     (increment = true) => {
       setTimeout(() => {
-        onSelectedIndexChange(((selectedIndex || tokens.length) + (increment ? 1 : -1)) % tokens.length);
+        onSelectedIndexChange((selectedIndex + (increment ? 1 : -1)) % tokens.length);
       }, 100); // Wait for indexChange event to be triggered
     },
     [tokens.length, onSelectedIndexChange, selectedIndex],
   );
 
   const currentToken = useMemo(
-    () =>
-      selectedIndex !== undefined ? (tokens.find(t => t.label === data[selectedIndex]?.label) ?? tokens[0]) : tokens[0],
+    () => tokens.find(t => t.label === data.at(selectedIndex)?.label) ?? tokens[0],
     [data, selectedIndex, tokens],
   );
 
-  return (
-    currentToken && (
-      <Drawer
-        size="large"
-        open={isOpen}
-        onClose={onClose}
-        onClick={e => e.stopPropagation()}
-        title={
-          <TabGroup
-            index={selectedIndex}
-            onIndexChange={isTokenListExpanded ? onSelectedIndexChange : undefined}
-            className="flex justify-center"
-          >
-            <TabList variant="line" onClick={e => e.stopPropagation()}>
-              <Flex className="gap-6">
-                {data.map(({ label }, i) => (
-                  <div className={isTokenListExpanded || selectedIndex === i ? 'block' : 'hidden'} key={label}>
-                    <Flex align="center">
-                      <IconChevronLeft
-                        className={twMerge('h-4 w-4 mr-2', !isTokenListExpanded ? 'block' : 'hidden')}
-                        onClick={() => changeToken(false)}
-                      />
-                      <Tab onClick={!isTokenListExpanded ? () => changeToken() : undefined}>{label}</Tab>
-                      <IconChevronRight
-                        className={twMerge('h-4 w-4 ml-2', !isTokenListExpanded ? 'block' : 'hidden')}
-                        onClick={() => changeToken(true)}
-                      />
-                    </Flex>
-                  </div>
-                ))}
-              </Flex>
-            </TabList>
-          </TabGroup>
-        }
-      >
-        <Flex vertical className="gap-4">
-          <Flex justify="space-between" align="center">
-            <Title>{t.price}</Title>
-            <LoadingMetric isReady={tokens.length > 0}>{getCurrency(tokens, currentToken.label)}</LoadingMetric>
-          </Flex>
-          <Flex justify="space-between" align="center">
-            <Title>{t.yearlyYield}</Title>
-            <RatioBadge data={currentToken.yearlyYield} />
-          </Flex>
-          <Flex vertical justify="space-between">
-            <CollapsiblePanel
-              className="text-justify"
-              isExpanded={false}
-              hasCardStyle={false}
-              label={<Title>{t.description}</Title>}
-            >
-              <Text className="break-words whitespace-normal overflow-y-auto">{currentToken.description}</Text>
-            </CollapsiblePanel>
-          </Flex>
-          <Flex justify="space-between" align="center">
-            <Title>
-              {t.volatility} / {t.risk}
-            </Title>
-            <Text type={getRisk(currentToken.volatility).type}>
-              {currentToken.volatility.toRatio(0)} / {getRisk(currentToken.volatility).label}
-            </Text>
-          </Flex>
-          <Flex justify="space-between" align="center">
-            <Title>{t.distribution}</Title>
-            <Text>{((data.find(t => t.label === currentToken.label)?.value || 0) / total).toRatio(0)}</Text>
-          </Flex>
-          <Flex justify="space-between" align="center">
-            <Title>{t.creation}</Title>
-            <Text>{currentToken.duration.formatDuration()}</Text>
-          </Flex>
-          <Flex justify="space-between" align="center">
-            <Title>{t.initPrice}</Title>
-            <Text>{currentToken.inceptionPrice.toLocaleCurrency()}</Text>
-          </Flex>
-          <Flex vertical className="gap-4" justify="space-between">
-            <CollapsiblePanel hasCardStyle={false} label={<Title>{t.historic}</Title>}>
-              <AreaChart
-                className="h-40"
-                data={tokenHistoric[selectedIndex || 0]}
-                categories={[t.amount]}
-                index="date"
-                colors={[
-                  tokenHistoric.length &&
-                  tokenHistoric[selectedIndex || 0][0].Montant < tokenHistoric[selectedIndex || 0][1].Montant
-                    ? 'green'
-                    : tokenHistoric.length &&
-                        tokenHistoric[selectedIndex || 0][0].Montant > tokenHistoric[selectedIndex || 0][1].Montant
-                      ? 'red'
-                      : 'white',
-                ]}
-                valueFormatter={number => number.toFixed(0)}
-                yAxisWidth={50}
-                showAnimation={true}
-                animationDuration={2000}
-                curveType="monotone"
-                noDataText={t.loading}
-                minValue={tokenHistoricLimit?.min ?? 0}
-                maxValue={tokenHistoricLimit?.max ?? 0}
-                showLegend={false}
-                startEndOnly={true}
-              />
-            </CollapsiblePanel>
-          </Flex>
+  return currentToken && data.length > 0 ? (
+    <Drawer
+      size="large"
+      open={isOpen}
+      onClose={onClose}
+      onClick={e => e.stopPropagation()}
+      title={
+        <TabGroup
+          index={selectedIndex}
+          onIndexChange={isTokenListExpanded ? onSelectedIndexChange : undefined}
+          className="flex justify-center"
+        >
+          <TabList variant="line" onClick={e => e.stopPropagation()}>
+            <Flex className="gap-6">
+              {data.map(({ label }, i) => (
+                <div className={isTokenListExpanded || selectedIndex === i ? 'block' : 'hidden'} key={label}>
+                  <Flex align="center">
+                    <IconChevronLeft
+                      className={twMerge('h-4 w-4 mr-2', !isTokenListExpanded ? 'block' : 'hidden')}
+                      onClick={() => changeToken(false)}
+                    />
+                    <Tab onClick={!isTokenListExpanded ? () => changeToken() : undefined}>{label}</Tab>
+                    <IconChevronRight
+                      className={twMerge('h-4 w-4 ml-2', !isTokenListExpanded ? 'block' : 'hidden')}
+                      onClick={() => changeToken(true)}
+                    />
+                  </Flex>
+                </div>
+              ))}
+            </Flex>
+          </TabList>
+        </TabGroup>
+      }
+    >
+      <Flex vertical className="gap-4">
+        <Flex justify="space-between" align="center">
+          <Title>{t.price}</Title>
+          <LoadingMetric isReady={tokens.length > 0}>{getCurrency(tokens, currentToken.label)}</LoadingMetric>
         </Flex>
-      </Drawer>
-    )
-  );
+        <Flex justify="space-between" align="center">
+          <Title>{t.yearlyYield}</Title>
+          <RatioBadge data={currentToken.yearlyYield} />
+        </Flex>
+        <Flex vertical justify="space-between">
+          <CollapsiblePanel
+            className="text-justify"
+            isExpanded={false}
+            hasCardStyle={false}
+            label={<Title>{t.description}</Title>}
+          >
+            <Text className="break-words whitespace-normal overflow-y-auto">{currentToken.description}</Text>
+          </CollapsiblePanel>
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Title>
+            {t.volatility} / {t.risk}
+          </Title>
+          <Text type={getRisk(currentToken.volatility).type}>
+            {currentToken.volatility.toRatio(0)} / {getRisk(currentToken.volatility).label}
+          </Text>
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Title>{t.distribution}</Title>
+          <Text>{((data.find(t => t.label === currentToken.label)?.value || 0) / total).toRatio(0)}</Text>
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Title>{t.creation}</Title>
+          <Text>{currentToken.duration.formatDuration()}</Text>
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Title>{t.initPrice}</Title>
+          <Text>{currentToken.inceptionPrice.toLocaleCurrency()}</Text>
+        </Flex>
+        <Flex vertical className="gap-4" justify="space-between">
+          <CollapsiblePanel hasCardStyle={false} label={<Title>{t.historic}</Title>}>
+            <AreaChart
+              className="h-40"
+              data={tokenHistoric[selectedIndex]}
+              categories={[t.amount]}
+              index="date"
+              colors={[
+                tokenHistoric.length &&
+                tokenHistoric[selectedIndex][0].Montant < tokenHistoric[selectedIndex][1].Montant
+                  ? 'green'
+                  : tokenHistoric.length &&
+                      tokenHistoric[selectedIndex][0].Montant > tokenHistoric[selectedIndex][1].Montant
+                    ? 'red'
+                    : 'white',
+              ]}
+              valueFormatter={number => number.toFixed(0)}
+              yAxisWidth={50}
+              showAnimation={true}
+              animationDuration={2000}
+              curveType="monotone"
+              noDataText={t.loading}
+              minValue={tokenHistoricLimit?.min ?? 0}
+              maxValue={tokenHistoricLimit?.max ?? 0}
+              showLegend={false}
+              startEndOnly={true}
+            />
+          </CollapsiblePanel>
+        </Flex>
+      </Flex>
+    </Drawer>
+  ) : null;
 };
