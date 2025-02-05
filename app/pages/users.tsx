4f91b0ee81c379b484330be5801773afb36bdd37
Flojito <flojito@archireport.com>
2025-02-05 03:48:03 +0100

    feat: add personal information to my profile

--- app/pages/users.tsx
+++ app/pages/users.tsx
@@ -1,37 +1,46 @@
-import { IconCopy, IconInfoCircleFilled, IconSearch } from '@tabler/icons-react';
-import { Icon, MultiSelect, MultiSelectItem, Switch, Table, TableBody, TableCell, TableRow } from '@tremor/react';
+import { IconCopy, IconSearch } from '@tabler/icons-react';
+import { MultiSelect, MultiSelectItem, Switch, Table, TableBody, TableCell, TableRow } from '@tremor/react';
+import { Card, Flex, message } from 'antd';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import SortTableHead from '../components/sortTableHead';
-import { Text, Title } from '../components/typography';
+import { Subtitle, Text, Title } from '../components/typography';
 import { useData } from '../hooks/useData';
 import { Page, useNavigation } from '../hooks/useNavigation';
 import { User, useUser } from '../hooks/useUser';
 import { getShortAddress } from '../utils/constants';
-import { isMobileDevice } from '../utils/mobile';
+import { useIsMobile } from '../utils/mobile';
 import { DataName, loadData } from '../utils/processData';
 import { Dataset } from '../utils/types';
-import { Card, Flex } from 'antd';
-import { message } from 'antd';
 
 const t: Dataset = {
+  myInfo: 'Mes informations',
   usersList: 'Liste des FiMseurs•es',
   noUserFound: 'Aucun FiMseur•se trouvée',
   shouldBecomePublic: 'Pour pouvoir voir les autres FiMseurs•es, votre profil doit être public',
   userLoading: 'Chargement des FiMseurs•es',
   selectUser: 'Sélectionner un FiMseur•se',
   search: 'Rechercher',
-  name: 'Nom',
-  address: 'Adresse',
+  name: 'Pseudo',
+  address: 'Adresse Solana',
   copy: 'Copier',
   addressCopied: 'Adresse copiée !',
   private: 'Privé',
   public: 'Public',
-  appearance: 'Etre visible des autres FiMseurs•es ?',
+  yes: 'Oui',
+  no: 'Non',
+  appearance: 'Visible des autres FiMseurs•es',
+  duration: 'FiMseur•se depuis',
+  donated: 'Montant des dons',
+  transferCost: 'Frais à rembourser',
 };
 
 export interface DBUser extends User {
   ispublic: boolean;
+  profitValue?: number;
+  transferCost?: number;
+  duration?: number;
+  donated?: number;
 }
 
 const thisPage = Page.Users;
@@ -41,6 +50,7 @@ export default function Users() {
   const { page, needRefresh, setNeedRefresh } = useNavigation();
   const { users, setUsers, isPublic, setIsPublic } = useData();
   const [messageApi, contextHolder] = message.useMessage();
+  const [myProfile, setMyProfile] = useState<DBUser>();
 
   const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
 
@@ -55,6 +65,9 @@ export default function Users() {
           .sort((a, b) => a.name.localeCompare(b.name))
           .sort((a, _) => (a.name === currentUser?.name ? -1 : 0)), // Put the current user on top
       );
+
+      const myProfile = users.find(user => user.name === currentUser?.name);
+      if (myProfile?.duration) setMyProfile(myProfile);
     },
     [currentUser?.name, setIsPublic, setUsers],
   );
@@ -99,8 +112,6 @@ export default function Users() {
     }
   }, [users]);
 
-  const [tooltipText, setTooltipText] = useState(t.appearance + ' ');
-
   const handleCopy = (address: string) => {
     navigator.clipboard.writeText(address);
     messageApi.open({
@@ -110,94 +121,127 @@ export default function Users() {
   };
 
   return (
-    <Card>
+    <Flex vertical className="gap-6">
       {contextHolder}
-      <Flex justify="space-between">
-        <Title className="text-left whitespace-nowrap">{t.usersList}</Title>
-        {isPublic !== undefined && (
-          <Flex justify="end" align="center">
-            <Icon
-              icon={IconInfoCircleFilled}
-              tooltip={tooltipText}
-              color="gray"
-              onClick={
-                isMobileDevice() ? () => setTooltipText(tooltipText !== t.appearance ? t.appearance : '') : undefined
-              }
-            />
-            <Text className="mr-2">{isPublic ? t.public : t.private}</Text>
-            <Switch disabled={isUpdatingUserPrivacy.current} checked={isPublic} onChange={handleSwitchChange} />
+      <Card>
+        <Title className="text-left whitespace-nowrap">{t.myInfo}</Title>
+        <Flex justify="space-between" align="center">
+          <Subtitle className="truncate whitespace-nowrap">{t.name}</Subtitle>
+          <Text>{currentUser?.name}</Text>
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Subtitle className="truncate whitespace-nowrap">{t.address}</Subtitle>
+          <Text>{useIsMobile() ? getShortAddress(currentUser?.address ?? '') : currentUser?.address}</Text>
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Subtitle className="truncate whitespace-nowrap">{t.appearance}</Subtitle>
+          {/* <Text>{isPublic ? t.yes : t.no}</Text> */}
+          {isPublic !== undefined ? (
+            <Flex justify="end" align="center">
+              <Text className="mr-2">{isPublic ? t.yes : t.no}</Text>
+              <Switch disabled={isUpdatingUserPrivacy.current} checked={isPublic} onChange={handleSwitchChange} />
+            </Flex>
+          ) : (
+            <div className="bg-theme-border rounded-md w-[70px] h-5 mb-1" />
+          )}
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Subtitle className="truncate whitespace-nowrap">{t.duration}</Subtitle>
+          {myProfile?.duration !== undefined ? (
+            <Text>{myProfile.duration.formatDuration()}</Text>
+          ) : (
+            <div className="bg-theme-border rounded-md w-24 h-5 mb-1" />
+          )}
+        </Flex>
+        <Flex justify="space-between" align="center">
+          <Subtitle className="truncate whitespace-nowrap">{t.donated}</Subtitle>
+          {myProfile?.donated !== undefined && myProfile?.profitValue !== undefined ? (
+            <Text>{`${myProfile.donated.toLocaleCurrency()} (${(myProfile.donated / myProfile.profitValue).toRatio()})`}</Text>
+          ) : (
+            <div className="bg-theme-border rounded-md w-32 h-5 mb-1" />
+          )}
+        </Flex>
+        {!!myProfile?.transferCost && (
+          <Flex justify="space-between" align="center">
+            <Subtitle className="truncate whitespace-nowrap">{t.transferCost}</Subtitle>
+            <Text className="font-bold">{myProfile?.transferCost?.toLocaleCurrency()}</Text>
           </Flex>
         )}
-      </Flex>
-      {isPublic === true ? (
-        <>
-          {users?.length && (
-            <Flex className="relative mt-5 max-w-md">
-              <label htmlFor="search" className="sr-only">
-                {t.searchByName}
-              </label>
-              <MultiSelect
-                autoFocus
-                ref={inputRef}
-                className="max-w-full sm:max-w-xs"
-                id="search"
-                icon={IconSearch}
-                placeholder={t.selectUser}
-                placeholderSearch={t.search}
-                spellCheck={false}
-                value={selectedUsers}
-                onValueChange={setSelectedUsers}
-              >
-                {users?.map(item => (
-                  <MultiSelectItem key={item.name} value={item.name}>
-                    {item.name}
-                  </MultiSelectItem>
-                ))}
-              </MultiSelect>
-            </Flex>
-          )}
-          <Table>
-            <SortTableHead labels={[t.name, t.address, t.copy]} table={users} setTable={setUsers} />
-            <TableBody>
-              {users?.length ? (
-                users.filter(isUserSelected).map(user => (
-                  <TableRow
-                    key={user.name}
-                    className={twMerge(
-                      'cursor-pointer',
-                      'hover:bg-theme-background-subtle dark:hover:bg-dark-theme-background-subtle',
-                      user.name === currentUser?.name ? 'bg-theme-border dark:bg-dark-theme-border' : '',
-                    )}
-                  >
-                    <TableCell>{user.name}</TableCell>
-                    <TableCell>
-                      <Text>{getShortAddress(user.address)}</Text>
-                    </TableCell>
-                    <TableCell>
-                      <IconCopy
-                        className={twMerge(
-                          'h-5 w-5 ml-3 cursor-pointer',
-                          'text-theme-content-subtle dark:text-dark-theme-content-subtle',
-                          'hover:text-theme-content dark:hover:text-dark-theme-content',
-                        )}
-                        onClick={() => handleCopy(user.address)}
-                      />
+      </Card>
+      <Card>
+        <Flex justify="space-between">
+          <Title className="text-left whitespace-nowrap">{t.usersList}</Title>
+        </Flex>
+        {isPublic === true ? (
+          <>
+            {users?.length && (
+              <Flex className="relative mt-5 max-w-md">
+                <label htmlFor="search" className="sr-only">
+                  {t.searchByName}
+                </label>
+                <MultiSelect
+                  autoFocus
+                  ref={inputRef}
+                  className="max-w-full sm:max-w-xs"
+                  id="search"
+                  icon={IconSearch}
+                  placeholder={t.selectUser}
+                  placeholderSearch={t.search}
+                  spellCheck={false}
+                  value={selectedUsers}
+                  onValueChange={setSelectedUsers}
+                >
+                  {users?.map(item => (
+                    <MultiSelectItem key={item.name} value={item.name}>
+                      {item.name}
+                    </MultiSelectItem>
+                  ))}
+                </MultiSelect>
+              </Flex>
+            )}
+            <Table>
+              <SortTableHead labels={[t.name, t.address, t.copy]} table={users} setTable={setUsers} />
+              <TableBody>
+                {users?.length ? (
+                  users.filter(isUserSelected).map(user => (
+                    <TableRow
+                      key={user.name}
+                      className={twMerge(
+                        'cursor-pointer',
+                        'hover:bg-theme-background-subtle dark:hover:bg-dark-theme-background-subtle',
+                        user.name === currentUser?.name ? 'bg-theme-border dark:bg-dark-theme-border' : '',
+                      )}
+                    >
+                      <TableCell>{user.name}</TableCell>
+                      <TableCell>
+                        <Text>{getShortAddress(user.address)}</Text>
+                      </TableCell>
+                      <TableCell>
+                        <IconCopy
+                          className={twMerge(
+                            'h-5 w-5 ml-3 cursor-pointer',
+                            'text-theme-content-subtle dark:text-dark-theme-content-subtle',
+                            'hover:text-theme-content dark:hover:text-dark-theme-content',
+                          )}
+                          onClick={() => handleCopy(user.address)}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={3} className="text-center">
+                      {users ? t.noUserFound : t.userLoading}
                     </TableCell>
                   </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={3} className="text-center">
-                    {users ? t.noUserFound : t.userLoading}
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </>
-      ) : (
-        <Text>{t.shouldBecomePublic}</Text>
-      )}
-    </Card>
+                )}
+              </TableBody>
+            </Table>
+          </>
+        ) : (
+          <Text>{t.shouldBecomePublic}</Text>
+        )}
+      </Card>
+    </Flex>
   );
 }
